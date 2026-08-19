b6a3cd67deec39d98b7fd3e50b9b78fd752cfad9
Maria Malmstedt Andersen <marmand@ulrik.uio.no>
2026-08-19 16:55:04 +0200

    Revert

--- script.js
+++ script.js
@@ -330,7 +330,6 @@ function initProjectPreviews(){
 
   projects.forEach((proj, idx) => {
     if (proj.dataset.previewInit === '1') return;
-    if (proj.dataset.continuousCase === '1') return;
 
     const scope = proj.querySelector('.project-inner') || proj;
 
